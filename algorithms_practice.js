6a24f5abd3669a7d7c7d3c0bde6595730ad167b5
Murodali <smurodali@gmail.com>
2022-03-10 22:56:29 +0500

    moreChallanges

--- algorithms_practice.js
+++ algorithms_practice.js
@@ -32,5 +32,54 @@ function longestString(arr){
 // console.log(longestString(["aaa","bbbbb","weqwdadasda","dss"]))
 
 function mostChar(str){
-    
+    let charsObj = {}
+    let maxCHar =""
+    let maxCharCount = 0
+
+    for(let i = 0; i < str.length; i++){
+        charsObj[str[i]] =++charsObj[str[i]] || 1
+    }
+
+    for( let key in charsObj){
+        if(charsObj[key] > maxCharCount){
+            maxCharCount = charsObj[key]
+            maxCHar = key
+        }
+    }
+    return maxCHar
 }
+
+// console.log(mostChar("hahhaaaahaa"))
+
+function maximumChar(arr){
+    let char
+    for(let i = 0; i < arr.length; i++){
+        let count = 0;
+        for(let j = 0; j < arr.length; j++){
+            if(arr[i] === arr[j]){
+                count++
+            }
+        }
+        if(count >1){
+            char = arr[i]
+        }
+    }
+    return char
+}
+
+// console.log(maximumChar("hahhaaaahaa"))
+
+function anagrams(str1, str2){
+    if(str1.length != str2.length){
+        return false
+    }
+
+    const sorted1 = sort(str1.split("")).join("");
+    const sorted2 = sort(str2.split("")).join("");
+
+    if(sorted1 === sorted2){
+        return true
+    }
+}
+
+// console.log(anagrams("angel","glean"))
